6be6d60cca93f61ee50b60de02c15ede3123129d
giovanazoker <vxmilakk@gmail.com>
2025-10-07 09:29:40 -0300

    Update 001.py

---
+++ giovana/003.py
@@ -0,0 +1,11 @@
+Primeiro = (int(input("Digite o primeiro valor: ")))
+Segundo = (int(input("Digite o segundo valor: ")))
+
+if Primeiro < Segundo:
+    print("O menor valor é:", Primeiro)
+    
+else:
+    print ("o menor valor é:", Segundo)
+
+if Primeiro == Segundo:
+    print("Os dois valores são iguais.")
