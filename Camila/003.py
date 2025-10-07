d77dcbe37cb1841ab0c3c725a5b0974c622f277f
Joanatas Fernandes Barbosa <94496092+joanatasFernandes@users.noreply.github.com>
2025-10-07 10:51:46 -0300

    update

--- Camila/003.py
+++ Camila/003.py
@@ -2,3 +2,11 @@ valor1 = float(input("Digite o primeiro valor: "))
 valor2 = float(input("Digite o segundo valor: "))
 maior = valor1 if valor1 > valor2 else valor2
 print(f"O maior valor é: {maior}")
+
+
+primeiro_valor = int( input("Digite um valor: "))
+segundo_valor = int( input("Digite um valor: "))
+print( "Primeiro" if primeiro_valor > segundo_valor else "Segundo")
+
+
+nome = ane ? jãoa
