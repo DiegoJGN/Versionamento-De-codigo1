c49c32ee52aa8b825a21e8e0b41efc7fc1cd58cb
Ryanspereira10 <ryanspereira10@Gmail.com>
2025-10-08 07:39:57 -0300

    005.py

--- Ryan/001.py
+++ Ryan/001.py
@@ -1,5 +1,3 @@
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
 ano_nasc = int(input("Digite o ano do seu nascimento: "))
 
 ano_atual = 2025
@@ -10,22 +8,3 @@ if idade >= 18:
     print("Maior de idade")
 else:
     print("Menor de idade")
-=======
-=======
->>>>>>> Stashed changes
-ano_nascimento = int(input("Digite o ano em que você nasceu: "))
-ano_nascimento = int(input("digite o ano em que voce nasceu:"))
-ano_nascimento = int(input("digite o ano em que voce nasceu:"))
-ano_atual = 2025
-idade = ano_atual - ano_nascimento
-if idade == 18:
-    print("Você faz 18 anos neste ano!")
-elif idade > 18:
-    print("Você já fez 18 anos.")
-else:
-<<<<<<< Updated upstream
-    print("Você ainda não fez 18 anos.")
->>>>>>> Stashed changes
-=======
-    print("Você ainda não fez 18 anos.")
->>>>>>> Stashed changes
