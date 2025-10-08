90fd075f54dfb17dc1dd98bcd0038f0b63c0a4ac
Diego Wilson <218095401+DiegoJGN@users.noreply.github.com>
2025-10-08 07:47:15 -0300

    Create 005.py

---
+++ Diego/005.py
@@ -0,0 +1,9 @@
+
+nomes_familia = ['Diego', 'Reinaldo', 'Laiza', 'João', 'Valentina', 'Joaquim']
+
+nomes_ordenados = sorted(nomes_familia)
+
+print('Nomes em ordem alfabética:')
+for nome in nomes_ordenados:
+    print(nome)
+
