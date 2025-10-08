424f12931124e7fd9deb3c1a6dff225299103f72
bestlice123-commits <219862931+giovanazoker@users.noreply.github.com>
2025-10-08 10:12:27 -0300

    Update 005.py

---
+++ giovana/005.py
@@ -0,0 +1,7 @@
+nomes_familia = ['Giovana', 'Maria', 'Luís', 'Admilson', 'Thiago', 'Júlia']
+
+nomes_ordenados = sorted(nomes_familia)
+
+print('Nomes em ordem alfabética:')
+for nome in nomes_ordenados:
+    print(nome)
