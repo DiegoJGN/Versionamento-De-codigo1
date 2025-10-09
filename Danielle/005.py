5bb96ae6939ae2e5804cdcc0d602a07f6a3bb3d4
danizoker <demoura.9998@gmail.com>
2025-10-09 10:13:24 -0300

    update 005.py

---
+++ Danielle/005.py
@@ -0,0 +1,7 @@
+nomes_familia = ['Danielle', 'keide', 'David', 'Maria Edurda']
+
+nomes_ordenados = sorted(nomes_familia)
+
+print('Nomes em ordem alfabética:')
+for nome in nomes_ordenados:
+    print(nome)
