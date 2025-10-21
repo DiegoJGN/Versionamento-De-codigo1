029ffd5b1b50203f56c891346fa41785c4c85278
João <joaovnf19022009@gmail.com>
2025-10-21 09:23:58 -0300

    00.pys

---
+++ João/005.py
@@ -0,0 +1,8 @@
+omes_familia = ['Diego', 'Reinaldo', 'Laiza', 'João', 'Valentina', 'Joaquim']
+
+nomes_ordenados = sorted(nomes_familia)
+
+print('Nomes em ordem alfabética:')
+for nome in nomes_ordenados:
+    print(nome)
+
---
+++ João/007.py
@@ -0,0 +1,5 @@
+idades = []
+for i in range(3):
+    nome = input(f"Digite o nome da {i+1}ª cidade: ")
+    cidades.append(nome) 
+    print("Lista atualizada de cidades:", cidades)  
