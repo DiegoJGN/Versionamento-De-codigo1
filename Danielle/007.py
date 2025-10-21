5bb3369cc45d42a37d44be69f63fe3266f7c50d5
danizoker <demoura.9998@gmail.com>
2025-10-21 11:22:36 -0300

    007.PY

--- Danielle/007.py
+++ Danielle/007.py
@@ -1,5 +1,9 @@
 cidades = []
-for i in range(3):
+for i in range(len(cidades)):
     nome = input(f"Digite o nome da {i+1}ª cidade: ")
-    cidades.append(nome) 
+    CLOSE = "SAIR"
+    if CLOSE == nome:
+        break
+    else:
+        cidades.append(nome) 
     print("Lista atualizada de cidades:", cidades) 
