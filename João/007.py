53fda8f5f189fbdbc8cfe0b3c1ae152198ae0322
Joao <joaovnf19022009@gmail.com>
2025-10-22 10:08:04 -0300

    Update 007.py

--- João/007.py
+++ João/007.py
@@ -1,5 +1,16 @@
-idades = []
-for i in range(3):
-    nome = input(f"Digite o nome da {i+1}ª cidade: ")
-    cidades.append(nome) 
-    print("Lista atualizada de cidades:", cidades)  
+print("⬐Lista das cidades adicionadas⬎")
+lista_cidades = []
+
+print("Escreva os nomes das cidades que você deseja adicionar.")
+print("Quando você acabar com a sua lista ou não tem mais o que fazer, digite 'final'.")
+
+while True:
+    cidade = input("Digite o nome de uma cidade: ")
+    if cidade.lower() == 'final' or cidade == '':
+        break
+    lista_cidades.append(cidade)
+
+print(lista_cidades)
+print("\nLista das cidades adicionadas:")
+for cidade in lista_cidades:
+    print(f" ⮕ {cidade}")
