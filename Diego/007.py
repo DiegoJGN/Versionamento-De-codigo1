14a93df67630b1bbc6738fb35bb3aec62aea2d09
Diego <dieguingithub@gmail.com>
2025-10-22 07:34:43 -0300

    Update 007.py

--- Diego/007.py
+++ Diego/007.py
@@ -1,5 +1,16 @@
-cidades = []
-for i in cidades:
-    nome = input("Digite o nome das cidades: ")
-    cidades.append(nome)
-    print("Essas foram as cidades citadas:", cidades)
+print("⬐Lista das cidades adicionadas⬎")
+lista_cidades = []
+
+print("Escreva os nomes das cidades que você deseja adicionar.")
+print("Quando você acabar com a sua lista ou não tem mais o que fazer, digite 'final'.")
+
+while True:
+    cidade = input("Digite o nome de uma cidade: ")
+    if cidade.lower() == 'final' or cidade == '':
+        break
+    lista_cidades.append(cidade)
+
+print(lista_cidades)
+print("\nLista das cidades adicionadas:")
+for cidade in lista_cidades:
+    print(f" ⮕ {cidade}")
