270547f68bbd531e5453a56bdc8dda274fb63b3f
henriiiq46 <ryanspereira10@Gmail.com>
2025-10-22 10:40:09 -0300

    Update 007.py

--- Camila/007.py
+++ Camila/007.py
@@ -1,5 +1,14 @@
 cidades = []
-for i in range(3):
-    nome = input(f"Digite o nome da {i+1}ª cidade: ")
-    cidades.append(nome) 
-    print("Lista atualizada de cidades:", cidades)  
+
+while True:
+    nome = input("Digite o nome de uma cidade (ou 'sair' para encerrar): ")
+    
+    if nome.lower() == 'sair':
+        print("Programa encerrado.")
+        break
+
+    cidades.append(nome)
+    
+    print("Lista de cidades atualizada:")
+    for cidade in cidades:
+        print("-", cidade)
